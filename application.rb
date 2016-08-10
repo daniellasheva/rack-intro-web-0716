5bd0ceed4cb6553d07860050ac3b8de8ad562266
daniellasheva <daniellasheva@gmail.com>
2016-08-10 16:35:57 -0400

    initial commit

--- application.rb
+++ application.rb
@@ -1,8 +1,15 @@
 class Application
 
-  def call(env)
+  def call(env) #code that runs whenever a request is received
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Daniella." 
+
+    # resp.write "This is my first webapp. \n"
+    # resp.write "It doesn't do much. \n\n"
+    # resp.write "But I think it's kind of cute.\n"
+    # resp.write "Most of all it just wants to say... \n\n\n\n"
+    # resp.write "///HAPPY BIRTHDAY TALIA!!\n\n\n\n"
+    # resp.write "P.S. Written to 'Arrival of the Birds' by the Cinematic Orchestra"
     resp.finish
   end
 
